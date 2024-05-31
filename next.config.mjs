1ce411ecc09a540870300ba81a87ec9e46f68deb
Devendra <dekuta75@gmail.com>
2024-05-31 23:51:01 +0530

    updated next.config.js-4

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
-// // @type {import('next').NextConfig}
-// const nextConfig = {
-//   rewrites: async () => {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         destination:
-//           process.env.NODE_ENV === "development"
-//             ? "http://127.0.0.1:8000/api/:path*"
-//             : "https://todoo-nine.vercel.app/api/:path*",
-//       },
-//     ];
-//   },
-// };
+// @type {import('next').NextConfig}
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/:path*",
+      },
+    ];
+  },
+};
 
-// export default nextConfig;
+export default nextConfig;
